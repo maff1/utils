219318d376f2657c80dbbd2ca79478a75f426caf
maff1 <49032010+maff1@users.noreply.github.com>
2023-02-21 14:55:22 +0000

    Add files via upload

---
+++ get_pep_stats.R
@@ -0,0 +1,54 @@
+#!/usr/bin/env Rscript
+library("optparse")
+library("dplyr")
+
+option_list = list(
+  make_option(c("-d", "--dir"), type="character", default=NULL,
+              help="directory for files", metavar="character"),
+  make_option(c("-o", "--out"), type="character", default="summary-stats.xlsx", 
+              help="output file name [default= %default]", metavar="character")
+)
+
+opt_parser = OptionParser(option_list=option_list)
+opt = parse_args(opt_parser)
+
+
+if (is.null(opt$dir)){
+  print_help(opt_parser)
+  stop("At least one argument must be supplied (input dir).n", call.=FALSE)
+}
+
+f_format_mean_sd <- function(svar, sdig, ssep, sns) {
+  paste(format(mean(svar, na.rm=T),digits=sdig, nsmall=sns), 
+        format(sd(svar, na.rm=T),digits=sdig, nsmall=sns), sep = ssep)
+}
+
+f_format_min_max <- function(svar, sdig, ssep, sns) {
+  paste(format(min(svar, na.rm=T),digits=sdig, nsmall=sns), 
+        format(max(svar, na.rm=T),digits=sdig, nsmall=sns), sep = ssep)
+}
+
+ls_files <- list.files(path = opt$dir, pattern = ".csv", recursive = TRUE, full.names = F)
+print(paste0(length(ls_files), " files found"))
+print(basename(ls_files))
+names(ls_files) <- substr(gsub("\\.\\/|\\/|\\.csv", "", ls_files), 1, 30)
+  ls_df <- lapply(ls_files, function (x) { 
+    df <- read.csv(x)
+    out_df <- df %>%
+     group_by(Source.File) %>%
+     summarise(
+       across(
+         where(~ is.numeric(.x)),
+           list(
+       "min.max" = ~ f_format_min_max(svar=.x, sdig=2, sns=2, ssep=":"),
+       "mean.sd" = ~ f_format_mean_sd(svar=.x, sdig=2, sns=2, ssep="+/-")
+           ), 
+         .names = "{.col}.{.fn}"),
+       "N.total.peptides" = n(),
+       "N.distinct.peptides" = n_distinct(Peptide)
+     )
+    out_df
+    }
+  )
+openxlsx::write.xlsx(ls_df, asTable = T, file = opt$out)
+print(paste0("summary-stats.xlsx", " in: ", getwd()))
